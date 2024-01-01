cf8e8c29b3acfa646a7f032fd51c09a5d092bd19
Patrick de Boer <patrickdeboer@me.com>
2024-01-01 21:52:31 +0100

    use org.apache.commons.math3 for matrix operations

--- hexapod/src/main/java/org/pdeboer/util/Matrix.java
+++ hexapod/src/main/java/org/pdeboer/util/Matrix.java
@@ -1,9 +1,9 @@
 package org.pdeboer.util;
 
-import static org.pdeboer.hexapod.Hexapod.*;
+import org.apache.commons.math3.linear.*;
 
 public record Matrix(
-		double[][] elements
+		RealMatrix matrix
 ) {
 
 	private final static int SIZE = 3;
@@ -12,18 +12,24 @@ public record Matrix(
 		this(new double[SIZE][SIZE]);
 	}
 
-	public double getElement(
+	public Matrix(final double[][] elements) {
+		this(MatrixUtils.createRealMatrix(elements));
+	}
+
+	public double getEntry(
 			int row,
 			int col) {
-		return elements[row][col];
+		return matrix.getEntry(row, col);
 	}
 
 	public Vector3d getRow(int idx) {
-		return new Vector3d(elements[idx][0], elements[idx][1], elements[idx][2]);
+		double[] row = matrix.getRow(idx);
+		return new Vector3d(row[0], row[1], row[2]);
 	}
 
 	public Vector3d getColumn(int idx) {
-		return new Vector3d(elements[0][idx], elements[1][idx], elements[2][idx]);
+		double[] row = matrix.getColumn(idx);
+		return new Vector3d(row[0], row[1], row[2]);
 	}
 
 	// clock-wise rotations
@@ -47,38 +53,40 @@ public record Matrix(
 
 	// rotate clockwise around x-axis
 	public static Matrix getRotateX(final double a) {
-		Matrix m = new Matrix();
-		m.elements[0][0] = 1;
-		m.elements[1][1] = Math.cos(a);
-		m.elements[1][2] = -Math.sin(a);
-		m.elements[2][1] = Math.sin(a);
-		m.elements[2][2] = Math.cos(a);
-		return m;
+		var elements = new double[SIZE][SIZE];
+		elements[0][0] = 1;
+		elements[1][1] = Math.cos(a);
+		elements[1][2] = -Math.sin(a);
+		elements[2][1] = Math.sin(a);
+		elements[2][2] = Math.cos(a);
+		return new Matrix(elements);
 	}
 
 	// rotate clockwise around y-axis
 	public static Matrix getRotateY(final double a) {
-		Matrix m = new Matrix();
-		m.elements[0][0] = Math.cos(a);
-		m.elements[0][2] = Math.sin(a);
-		m.elements[1][1] = 1;
-		m.elements[2][0] = -Math.sin(a);
-		m.elements[2][2] = Math.cos(a);
-		return m;
+		var elements = new double[SIZE][SIZE];
+		elements[0][0] = Math.cos(a);
+		elements[0][2] = Math.sin(a);
+		elements[1][1] = 1;
+		elements[2][0] = -Math.sin(a);
+		elements[2][2] = Math.cos(a);
+		return new Matrix(elements);
 	}
 
 	// rotate clockwise around z-axis
 	public static Matrix getRotateZ(final double a) {
-		Matrix m = new Matrix();
-		m.elements[0][0] = Math.cos(a);
-		m.elements[0][1] = -Math.sin(a);
-		m.elements[1][0] = Math.sin(a);
-		m.elements[1][1] = Math.cos(a);
-		m.elements[2][2] = 1;
-		return m;
+		var elements = new double[SIZE][SIZE];
+		elements[0][0] = Math.cos(a);
+		elements[0][1] = -Math.sin(a);
+		elements[1][0] = Math.sin(a);
+		elements[1][1] = Math.cos(a);
+		elements[2][2] = 1;
+		return new Matrix(elements);
 	}
 
 	public Vector3d multiply(final Vector3d v) {
+		var elements = matrix.getData();
+
 		Vector3d r = new Vector3d();
 		r.x = elements[0][0] * v.x + elements[0][1] * v.y + elements[0][2] * v.z;
 		r.y = elements[1][0] * v.x + elements[1][1] * v.y + elements[1][2] * v.z;
@@ -87,19 +95,7 @@ public record Matrix(
 	}
 
 	public Matrix multiply(final Matrix m) {
-		Matrix nm = new Matrix();
-		for (int row = 0; row < SIZE; ++row) {
-			double[] er = elements[row];
-			for (int col = 0; col < SIZE; ++col) {
-				double sum = 0;
-				for (int idx = 0; idx < SIZE; ++idx) {
-					sum += er[idx] * m.elements[idx][col];
-				}
-				nm.elements[row][col] = sum;
-			}
-		}
-
-		return nm;
+		return new Matrix(matrix.multiply(m.matrix));
 	}
 
 	public Matrix rotateX(final double a) {
